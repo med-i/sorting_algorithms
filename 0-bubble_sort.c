04a7df4a1d65dfc96cf1afdfc127353f720f7400
meriembenayad <meriem.benayad@gmail.com>
2023-09-21 19:09:36 +0100

    Fix Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 /**
  * swap - Swap two element
  * @a: pointer to the 1s element to swap
- * @b: ponter to the 2d element
+ * @b: pointer to the 2d element
  */
 void swap(int *a, int *b)
 {
@@ -24,13 +24,16 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(array + j, array + j + 1);
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
