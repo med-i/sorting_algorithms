4bbc64e4638ac548a5274d561d358aa6fccaeb21
meriembenayad <meriem.benayad@gmail.com>
2023-09-21 19:00:06 +0100

    Bubble & Insertion Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - Swap two element
+ * @a: pointer to the 1s element to swap
+ * @b: ponter to the 2d element
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - sorts an array of integers in ascending order
  * using the Bubble sort algorithm
  * @array: pointer to the array to be sorted
@@ -8,4 +22,17 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,55 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - swaps two nodes of doubly linkedlist
+ * @list: pointer to pointer to doubly linked list
+ * @node1: pointer to 1st node
+ * @node2: ponter to 2s node
+ */
+void swap_nodes(listint_t **list, listint_t *node)
+{
+	listint_t *prev, *next;
+
+	prev = node->prev;
+	next = node->next;
+
+	prev->next = next;
+	if (next)
+		next->prev = prev;
+
+	node->prev = prev->prev;
+	node->next = prev;
+
+	if (prev->prev)
+		prev->prev->next = node;
+	else
+		*list = node;
+	prev->prev = node;
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: double pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *prev;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		prev = current->prev;
+		while (prev != NULL && current->n < prev->n)
+		{
+			swap_nodes(list, current);
+			prev = current->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
